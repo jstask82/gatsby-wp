8cff920f45cf600cb866942e055c11446ff4ad6e
Aaron Kessler <jstask@me.com>
2020-11-02 20:33:20 +0100

    fix internal links

--- src/components/common/Link.js
+++ src/components/common/Link.js
@@ -8,6 +8,7 @@ const Link = ({ children, to, activeClassName, partiallyActive, ...other }) => {
   // Tailor the following test to your environment.
   // This example assumes that any internal link (intended for Gatsby)
   // will start with exactly one slash, and that anything else is external.
+  // TODO Check for baseUrl also
   const internal = /^\/(?!\/)/.test(to)
 
   // Use Gatsby Link for internal links, and <a> for others
